878f4a7251d7459055c80079846e1e897c51ca16
Tony Wu <tony.wu@berkeley.edu>
2016-03-03 22:26:52 -0800

    Initial run.py with arg parser

---
+++ run.py
@@ -0,0 +1,16 @@
+import argparse
+
+parser = argparse.ArgumentParser(description='On-demand price simulator')
+parser.add_argument(
+	'--num_drivers', 
+	type=int,
+	help='number of initial drivers (default: 50)'
+	)
+parser.add_argument(
+	'--num_passengers',
+	type=int,
+	help='number of initial passengers (default: 50)')
+
+args = parser.parse_args()
+print(args.num_drivers)
+print(args.num_passengers)
